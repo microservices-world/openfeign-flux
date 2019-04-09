72ac00346794e17b49f5bc330b5360483a673d28
Zhenglai Zhang <zhenglai.zhang@bkjk.com>
2019-04-10 00:41:10 +0800

    test openfeign-flux 0.1

--- src/main/java/org/ms/openfeignflux/test/TestController.java
+++ src/main/java/org/ms/openfeignflux/test/TestController.java
@@ -3,7 +3,6 @@ package org.ms.openfeignflux.test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 /**
  * @author Zhenglai
@@ -17,10 +16,10 @@ public class TestController {
 
     @GetMapping("/")
     public void test() {
-        var users = userApi.getAllUser();
-        userApi.getUser("12313");
-        userApi.deleteUser("3121");
-        userApi.createUser(Mono.just(new User()));
+//        var users = userApi.getAllUser();
+//        userApi.getUser("5cab8a5c8b0b783897fd2e19");
+        userApi.deleteUser("5cab8a7c8b0b783897fd2e1a").subscribe(x -> System.out.println("delete success"));
+//        userApi.createUser(Mono.just(new User()));
 //        users.subscribe(System.out::println);
     }
 }
